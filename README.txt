b566f53be172fd05cd9079596630be8815f8b001
Jack Schiffman <jschiffman2017@pingry.org>
2016-03-09 20:11:35 -0500

    Initial commit.

---
+++ README.txt
@@ -0,0 +1,3 @@
+Written by: Jack Schiffman
+
+User inputs a base and an exponent. Programs prints the value of the base raised to the exponent.
---
+++ baseExponent.py
@@ -0,0 +1,16 @@
+def power(base, exponent):
+	if exponent == 0:
+		return(1)
+	else:
+		return(base * power(base, exponent - 1))
+		
+userBase = input("What is the base? ")
+userBase = float(userBase)
+userExponent = input("What is the exponent? ") 
+userExponent = float(userExponent)
+
+userPower = power(userBase, userExponent)
+
+print(userPower)
+
+ 	
